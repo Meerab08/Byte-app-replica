503cd22986c77c157992107cc2081c5c285c2689
Meerab08 <ameerab.rafique@devsinc.com>
2022-12-12 19:57:50 +0500

    set URL in environment variable

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 //    Let’s connect our database to MongoDB Atlas
 // ************************************************
 module.exports = {
-  url: "mongodb+srv://meerab:meerab12345@cluster0.dqzdvrm.mongodb.net/?retryWrites=true&w=majority",
+  url: process.env.URL,
 };
 
 // This file has our DB link to MongoDB Atlas
